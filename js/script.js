78d630c313eef0ab0a0d9a235102653d4ef2d85e
thienv29 <thienv29@gmail.com>
2023-02-12 17:03:34 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -209,4 +209,4 @@ function setColorIconLang(primaryColor) {
 
 
 $('#link-domain').innerHTML=`${window.location.hostname}`
-$('#link-domain').setAttribute('href', `${window.location.hostname}`);
+$('#link-domain').setAttribute('href', `https://${window.location.hostname}`);
